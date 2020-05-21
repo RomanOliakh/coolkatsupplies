2eca2119aecc3319e56773df7e3a48c53f3a3dde
RomanOliakh <37980547+RomanOliakh@users.noreply.github.com>
2020-05-21 19:52:38 +0300

    Add files via upload

---
+++ coolkatbbbsystem.js
@@ -0,0 +1,172 @@
+
+    /*Order Information START*/
+
+    $(document).ready(function () {
+        var orderID = function () {
+            var text = "";
+            var possible = "abdegijklmopqrstuvwxyz0123456789";
+
+            for (var i = 0; i < 5; i++)
+                text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+            return text;
+        }
+
+        $('.refresh').click(function () {
+            window.location.reload(true);
+        });
+
+        $('#proceed').click(function () {
+            $('.balloons .b1:visible, .balloons .b2:visible, .balloons .b3:visible, .balloons .b4:visible, .balloons .b5:visible').each(function () {
+                $(this).removeClass('active');
+                var getDesc = $(this).attr('class');
+                var useDesc = getDesc.split(' ');
+                $('.desc_row-1').append('<br/>| ' + getDesc);
+            });
+            if ($('#top').is(':visible')) {
+                var getTop = $('.balloons #top:visible').attr('class');
+                var useTop = getTop.split(' ');
+                $('.desc_top').append('<br/>| ' + getTop);
+            }
+            if ($('.build-extra').hasClass('3ft-tassel')) {
+                var getTas = $('#ribbonchange').attr('class');
+                $('.desc_acc').append('<br/>| ' + getTas);
+
+            }
+            $('.finaltitle').show();
+            $('#orderID').html(orderID);
+        });
+
+        $('#inputquantity').change(function () {
+            var costCalc = ($('#inputquantity').val()) * ($('.costCalc').text());
+            $('.finalCost').html("$" + costCalc.toFixed(2));
+        });
+
+        $('#finalize').click(function () {
+            var provide = document.querySelector('input[name="provide"]:checked').value;
+            $(this).fadeOut(1000);
+
+            var data =
+                {
+                    name: $("#name").val(),
+                    orderID: $("#orderID").text(),
+                    quantity: $("#inputquantity").val(),
+                    cost: $("#totalvalue").text(),
+                    costCalc: $(".finalCost").text(),
+                    deliver: $('input[name="provide"]:checked').val(),
+                    deliveryaddress: $("#deliveryaddress").val(),
+                    deliverycity: $("#deliverycity").val(),
+                    deliverypostal: $("#deliverypostal").val(),
+                    date: $("#inputdate").val(),
+                    time: $("#inputtime").val(),
+                    email: $("#email").val(),
+                    message: $("#message").val(),
+                    temBal: $(".desc_template").text(),
+                    topBal: $(".desc_top").text(),
+                    bodBal: $(".desc_row-1").text(),
+                    accessories: $(".desc_acc").text(),
+                };
+            $.ajax(
+                {
+                    type: "POST",
+                    url: "send.php",
+                    data: data,
+                    success: function () {
+                        $('#progress, #information').hide();
+                        $('.success, #finished').show();
+                    }
+                });
+        });
+    });
+
+/*Order Information END*/
+
+
+
+/*Change BG Img Webflow CMS START*/
+
+$(document).ready(function() {
+    jQuery.fn.removeClassExcept = function (val) {
+        return this.each(function (index, el) {
+            var keep = val.split(" "),  // list we'd like to keep
+                reAdd = [],          // ones that should be re-added if found
+                $el = $(el);       // element we're working on
+            // look for which we re-add (based on them already existing)
+            for (var c = 0; c < keep.length; c++){
+                if ($el.hasClass(keep[c])) reAdd.push(keep[c]);
+            }
+            // drop all, and only add those confirmed as existing
+            $el
+                .removeClass()               // remove existing classes
+                .addClass(reAdd.join(' '));  // re-add the confirmed ones
+        });
+    };
+
+    $('.sample').click(function() {
+        var getColor = $(this).attr('class');
+        var useColor = getColor.split(' ');
+
+        $('.active').removeClassExcept("b1 b2 b3 b4 b5 b6 b7 b8 b9 b10 b11 b12 b13 b14 b15 b16 b17 b18 b19 b20 top alt single-string active");
+        $('.active').addClass([useColor[1], useColor[2]].join(" ") );
+
+    });
+
+
+
+
+
+});
+/*Change BG Img Webflow CMS END*/
+
+
+
+/*Click to (toggle) Active Balloon START*/
+
+$('.b1, .b2, .b3, .b4, .b5, .b6, .b7, .b8, .b9, .b10, .b11, .b12, .b13, .b14, .b15, .b16, .b17, .b18, .b19, .b20').click(function() {
+    $(this).toggleClass('active');
+    if ($('.top').hasClass('active')) {
+        $('.top').removeClass('active');
+        $('.folder').hide();
+    }
+});
+$('.top').click(function() {
+    $(this).toggleClass('active');
+    $('.b1, .b2, .b3, .b4, .b5, .b6, .b7, .b8, .b9, .b10, .b11, .b12, .b13, .b14, .b15, .b16, .b17, .b18, .b19, .b20').removeClass('active');
+    $('.folder').hide();
+});
+
+/*Click to (toggle) Active Balloon END*/
+
+
+
+/* Click to select-deselect ALL balloon START*/
+
+$('#opt-select').click(function () {
+    $('.b1, .b2, .b3, .b4, .b5, .b6, .b7, .b8, .b9, .b10, .b11, .b12, .b13, .b14, .b15, .b16, .b17, .b18, .b19, .b20, .top').addClass('active');
+});
+
+$('#opt-deselect').click(function () {
+    $('.active').removeClass('active');
+});
+
+/* Click to select-deselect ALL balloon END*/
+
+
+
+/* Click to ADD-REMOVE balloon START*/
+
+$('#opt-sub').click(function () {
+    $(".balloons > [class*='r']:visible:first").hide();
+    $(".select > [class*='opt-r']:visible:first").hide(100);
+});
+
+$('#opt-add').click(function () {
+    $(".balloons > [class*='r']:hidden:last").show();
+    $(".select > [class*='opt-r']:hidden:last").show(100);
+
+});
+
+/* Click to ADD-REMOVE balloon END*/
+
+
+
